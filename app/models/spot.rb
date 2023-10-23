90b92c5716ac02688ade8d13be3f4d82b7c39772
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-23 17:03:18 +0900

    バリデーション設定(Spot)

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,11 @@
 class Spot < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :address
+    validates :latitude
+    validates :longitude
+  end
+
 end

d067dbb345e4c195658161c488bbbe680e550f76
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-25 15:33:14 +0900

    タグ管理機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "spots#index"
   resources :spots, only: [:new, :create]
+  resources :tags, only: [:index, :new, :create, :edit, :update]
 end

8d2f38f20fa4c5560150f5e841786df86b7885d5
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-25 18:14:44 +0900

    ビューファイル編集
    新規投稿画面にチェックボックス（複数選択可）を表示

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,12 +1,26 @@
 class SpotsController < ApplicationController
 
   def index
+    @spots = Spot.all
   end
 
   def new
+    @spot = Spot.new
   end
 
   def create
+    @spot = Spot.new(spot_params)
+    if @spot.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spot_params
+    params.require(:post).permit(:name, :address, :latitude, :longitude, tag_ids: []).merge(user_id: current_user.id)
   end
   
 end

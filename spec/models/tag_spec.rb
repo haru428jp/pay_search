b6ce3d5c957a4280b733f30274f6979c203c6176
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-25 22:57:02 +0900

    単体テストコード（タグ登録）

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.build(:tag)
+  end
+
+  describe 'タグ登録' do
+    context 'タグ登録できる場合' do
+      it 'nameが存在すれば登録できる' do
+        expect(@tag).to be_valid
+      end
+    end
+
+    context 'タグ登録できない場合' do
+      it 'nameが空では登録できない' do
+        @tag.name = ''
+        @tag.valid?
+        expect(@tag.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end

57e7976f7628c6428fdbe672ce8454ca6de50490
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-26 17:19:33 +0900

    ルーティング設定(edit,update)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "spots#index"
-  resources :spots, only: [:new, :create, :show]
+  resources :spots, only: [:new, :create, :show, :edit, :update]
   resources :tags, only: [:index, :new, :create, :edit, :update]
 end

c71ebc10f618fccc8459858203bb467c605e0204
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-28 23:24:23 +0900

    単体テストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    comment { Faker::Lorem.sentence }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @spot = FactoryBot.create(:spot)
+    @comment = FactoryBot.build(:comment, user_id: @user.id, spot_id: @spot.id)
+  end
+
+  describe 'コメント登録' do
+    context 'コメント登録できる場合' do
+      it 'commentが存在すれば登録できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント登録できない場合' do
+      it 'commentが空では登録できない' do
+        @comment.name = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Comment can't be blank")
+      end
+    end
+  end
 end

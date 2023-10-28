849257732d616124cdfde397cb0c9c82b946728a
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-28 18:25:00 +0900

    モデルとテーブルを作成(comment)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_084422) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_091104) do
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "comment", null: false
+    t.bigint "user_id", null: false
+    t.bigint "spot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_comments_on_spot_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "spot_tags", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "spot_id", null: false
     t.bigint "tag_id", null: false
@@ -51,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_27_084422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "spots"
+  add_foreign_key "comments", "users"
   add_foreign_key "spot_tags", "spots"
   add_foreign_key "spot_tags", "tags"
   add_foreign_key "spots", "users"

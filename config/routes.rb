71e5c57c40b188de7e6948f5db44f1e682d66df0
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-28 18:32:19 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "spots#index"
   resources :spots do
+    resources :comments, only: :create
     collection do
       get 'search'
     end

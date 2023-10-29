6a65d9b74d3be38ab921739f82472621b38ca81a
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-29 20:55:45 +0900

    マイページ編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+
   def show
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id || current_user.admin?
+      redirect_to root_path
+    end
   end
+
 end

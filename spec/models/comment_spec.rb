e9f08a3c83ff36cf0fafc112a7cc436edbb29c8f
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-31 14:35:52 +0900

    単体テストコード修正(comment)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Comment, type: :model do
       it 'commentが空では登録できない' do
         @comment.comment = ''
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("Commentを入力してください")
+        expect(@comment.errors.full_messages).to include("コメントを入力してください")
       end
     end
   end
